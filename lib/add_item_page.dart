5732d020d3493f53fff3f19ebe062c40e6f08495
Denis Svirin <welkopr9d@gmail.com>
2023-10-17 14:37:46 +0200

    adding

--- lib/add_item_page.dart
+++ lib/add_item_page.dart
@@ -1,6 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'main.dart';
 
 class AddNewItemPage extends StatelessWidget {
+  final TextEditingController _addNewItemController = TextEditingController();
+  final TextEditingController _addNewTagController = TextEditingController();
+
+  void _add_new_item(context, item_to_check, tags) async {
+    final String endpoint = 'http://localhost:3000/check_items/add_new.json';
+
+    try {
+      final response = await http.post(
+        Uri.parse(endpoint),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode({"item_to_check": item_to_check, "tags": tags}),
+      );
+      if (response.statusCode == 204) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SearchPage()),
+        );
+      } else {}
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +41,14 @@ class AddNewItemPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             TextField(
+              controller: _addNewItemController,
               decoration: InputDecoration(
                 labelText: 'Item to check',
               ),
             ),
             SizedBox(height: 20),
             TextField(
+              controller: _addNewTagController,
               decoration: InputDecoration(
                 labelText: 'Tags',
               ),
@@ -27,7 +56,9 @@ class AddNewItemPage extends StatelessWidget {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Действие при сохранении данных.
+                var itemToCheck = _addNewItemController.text;
+                var tags = _addNewTagController.text;
+                _add_new_item(context, itemToCheck, tags);
               },
               child: Text('Save'),
             ),
